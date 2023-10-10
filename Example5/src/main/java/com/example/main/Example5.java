718a97f3851af872af235208122f7e5ca153784c
Kiran D Chavan <137285510+KiranChavan18@users.noreply.github.com>
2023-10-10 22:56:27 +0530

    Update and rename Example6.java to Example5.java

--- Example5/src/main/java/com/example/main/Example5.java
+++ Example5/src/main/java/com/example/main/Example5.java
@@ -2,7 +2,7 @@ package com.example.main;
 import  com.example.beans.Vehicle;
 import com.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-public class Example6 {
+public class Example5 {
     public static void main(String[] args) {
         var context= new AnnotationConfigApplicationContext(ProjectConfig.class);
         Vehicle vehicle = context.getBean(Vehicle.class);
